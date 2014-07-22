3a05c6add7aa7327e752847b7ede9c9f5fda9de5
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2014-07-21 22:16:46 -0400

    Commented out/removed WebSocket for now.

--- org.nasdanika.web/src/org/nasdanika/web/WebSocketContext.java
+++ org.nasdanika.web/src/org/nasdanika/web/WebSocketContext.java
@@ -1,10 +1,10 @@
 package org.nasdanika.web;
 
-import org.eclipse.jetty.websocket.WebSocket.Connection;
-
-
-public interface WebSocketContext extends WebContext {
-
-	Connection getConnection();
-	
-}
+//import org.eclipse.jetty.websocket.WebSocket.Connection;
+//
+//
+//public interface WebSocketContext extends WebContext {
+//
+//	Connection getConnection();
+//	
+//}
