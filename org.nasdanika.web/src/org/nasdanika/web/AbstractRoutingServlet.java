916982814419720db2b50dcf73cb1ed59ca2bb62
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2015-05-13 21:44:22 -0400

    Code cleanup

---
+++ org.nasdanika.web/src/org/nasdanika/web/AbstractRoutingServlet.java
@@ -0,0 +1,162 @@
+package org.nasdanika.web;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URL;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.nasdanika.core.AuthorizationProvider.AccessDecision;
+
+@SuppressWarnings("serial")
+public abstract class AbstractRoutingServlet extends HttpServlet {
+	
+	protected ExtensionManager extensionManager;
+	
+	private boolean jsonPrettyPrint;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		jsonPrettyPrint = "true".equals(config.getInitParameter("json-pretty-print"));
+		try {
+			extensionManager = new ExtensionManager(
+					this,
+					null, 
+					config.getInitParameter("adapter-service-filter"),
+					config.getInitParameter("route-service-filter"),
+					config.getInitParameter("ui-part-service-filter"),
+					config.getInitParameter("html-factory"),
+					"deny".equalsIgnoreCase(config.getInitParameter("default-access-decision")) ? AccessDecision.DENY : AccessDecision.ALLOW);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}		
+	}
+			
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String reqUrl = req.getContextPath()+req.getServletPath();
+		String pathInfo = req.getPathInfo();		
+		
+		if (pathInfo==null) {
+			pathInfo = "";
+		}
+		if (pathInfo.startsWith("/")) {
+			pathInfo = pathInfo.substring(1);
+		}
+		String[] path = pathInfo.split("/");
+		try (HttpServletRequestContext context = createContext(path, req, resp, reqUrl)) {
+			for (Route route: extensionManager.getRouteRegistry().matchRootRoutes(RequestMethod.valueOf(req.getMethod()), path)) {
+				try (Action action = route.execute(context)) {
+					if (action!=null) {
+						resultToResponse(action.execute(), resp, context);
+						return;
+					}
+				}
+			}
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+		} catch (ServerException e) {
+			resp.sendError(e.getStatusCode(), e.getMessage());
+		} catch (ServletException | IOException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new ServletException(e);			
+		}
+	}
+
+	protected abstract HttpServletRequestContext createContext(
+			String[] path,
+			HttpServletRequest req, 
+			HttpServletResponse resp, 
+			String reqUrl) throws Exception;
+
+	public void resultToResponse(Object result, HttpServletResponse resp, HttpServletRequestContext context) throws Exception {
+		if (result instanceof ProcessingError) {
+			ProcessingError processingError = (ProcessingError) result;
+			if (processingError.getMessage()==null) {
+				resp.sendError(processingError.getCode());
+			} else {
+				resp.sendError(processingError.getCode(), processingError.getMessage());
+			}
+		} else if (result instanceof JSONArray) {
+			resp.setContentType("application/json");
+			if (jsonPrettyPrint) {
+				resp.getWriter().write(((JSONArray) result).toString(4));
+			} else {
+				((JSONArray) result).write(resp.getWriter());
+			}
+		} else if (result instanceof JSONObject) {
+			resp.setContentType("application/json");
+			if (jsonPrettyPrint) {
+				resp.getWriter().write(((JSONObject) result).toString(4));
+			} else {
+				((JSONObject) result).write(resp.getWriter());
+			}
+		} else if (result instanceof String) {
+			resp.getWriter().write((String) result);
+		} else if (result instanceof char[]) {
+			resp.getWriter().write((char[]) result);
+		} else if (result instanceof byte[]) {
+			resp.getOutputStream().write((byte[]) result);
+		} else if (result instanceof Reader) {
+			try {
+				Writer writer = resp.getWriter();
+				char[] cbuf = new char[4096];
+				int l;
+				while ((l=((Reader) result).read(cbuf))!=-1) {
+					writer.write(cbuf, 0, l);
+				}
+			} finally {
+				((Reader) result).close();
+			}
+		} else if (result instanceof InputStream) {
+			try (InputStream in = (InputStream) result; OutputStream out = resp.getOutputStream()) {
+				byte[] buf = new byte[4096];
+				int l;
+				while ((l=((InputStream) result).read(buf))!=-1) {
+					out.write(buf, 0, l);
+				}
+			}
+		} else if (result instanceof URL) {
+			resultToResponse(((URL) result).openStream(), resp, context);
+		} else if (result!=null) {
+			JSONObject jsonResult = context.convert(result, JSONObject.class);
+			if (jsonResult == null || jsonResult.length()==0) {
+				try {
+					JSONArray jaResult = context.convert(result, JSONArray.class);
+					if (jaResult==null) {
+						resp.getWriter().write(String.valueOf(result));
+					} else {
+						resultToResponse(jaResult, resp, context);					
+					}
+				} catch (Exception e) {
+					resp.getWriter().write(String.valueOf(result));					
+				}
+			} else {
+				resultToResponse(jsonResult, resp, context);
+			}
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		if (extensionManager!=null) {
+			try {
+				extensionManager.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		super.destroy();
+	}
+
+}
