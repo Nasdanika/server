d7b51b5332e66131ebe5824be0cc3832d60c060f
Pavel <Pavel.Vlasov@nasdanika.org>
2015-12-16 23:25:27 -0500

    Wait is repeatable.

--- org.nasdanika.webtest/src/org/nasdanika/webtest/Wait.java
+++ org.nasdanika.webtest/src/org/nasdanika/webtest/Wait.java
@@ -1,6 +1,7 @@
 package org.nasdanika.webtest;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -20,6 +21,7 @@ import org.openqa.selenium.support.How;
  * {@link org.openqa.selenium.By} methods in By class.
  * 
  */
+@Repeatable(Waits.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE })
 public @interface Wait {
