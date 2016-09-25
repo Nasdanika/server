92759a8e1b8e5501469602ca63a564c262c0d871
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2016-09-25 17:09:08 -0400

    Category is inherited.

--- org.nasdanika.webtest/src/org/nasdanika/webtest/Category.java
+++ org.nasdanika.webtest/src/org/nasdanika/webtest/Category.java
@@ -1,6 +1,7 @@
 package org.nasdanika.webtest;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -13,6 +14,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface Category {
 
 	/**
