1078abae8832c5de73011d26e314ef8b9e2d605d
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2016-11-24 10:41:47 -0500

    Codegen has been moved to its own repository.

--- org.nasdanika.codegen/src/org/nasdanika/codegen/IFilter.java
+++
@@ -1,9 +0,0 @@
-package org.nasdanika.codegen;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-
-public interface IFilter<T> {
-
-	T filter(T input, IProgressMonitor monitor) throws Exception;
-	
-}
