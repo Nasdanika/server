448f7b13b4a1123073d121ed0be616c08a5ec2fd
Pavel <Pavel.Vlasov@nasdanika.org>
2017-01-13 17:19:47 -0500

    Refactored AuthorizationHelper to the new model.

--- org.nasdanika.cdo.security/src/org/nasdanika/cdo/security/AuthorizationHelper.java
+++ org.nasdanika.cdo.security/src/org/nasdanika/cdo/security/AuthorizationHelper.java
@@ -1,198 +1,62 @@
 package org.nasdanika.cdo.security;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.nasdanika.core.AuthorizationProvider.AccessDecision;
 import org.nasdanika.core.Context;
-import org.nasdanika.core.CoreUtil;
 
 /**
- * Helper class for resolving permissions.
+ * Helper (mix-in) class for resolving permissions.
  * @author Pavel
  *
  */
 public class AuthorizationHelper {
 
-	public static final String ANNOTATION_PERMISSIONS = "org.nasdanika.cdo.security:permissions";
-	private static final String SLASH = "/";
 	private static final String PARENT_NAVIGATION = "../";
 	private Principal principal;
 	
+	/**
+	 * Creates authorization helper for a given principal.
+	 * @param principal
+	 */
 	public AuthorizationHelper(Principal principal) {
 		this.principal = principal;
 	}
 	
+	/**
+	 * Authorizes the helper's principal to perform an action against specified target in
+	 * @param context
+	 * @param target
+	 * @param action
+	 * @param qualifier
+	 * @param environment
+	 * @return
+	 */
 	public AccessDecision authorize(Context context, EObject target, String action, String qualifier, Map<String, Object> environment) {
 		if (target!=null) {
 			environment = environment == null ? new HashMap<String, Object>() : new HashMap<String, Object>(environment); 
 			environment.put("target", target);
 		}
-		return authorize(principal, context, target, action, qualifier, SLASH, environment, new HashSet<Principal>());
+		return authorize(principal, context, target, action, qualifier, new ArrayList<>(), environment, new HashSet<Principal>());
 	}
-	
-	private static AccessDecision authorize(
-			Principal principal, 
-			Context context,
-			EObject target, 
-			String action,
-			String path, 
-			Map<String, Object> environment, 
-			Set<Principal> traversed) {
 		
-//		System.out.println("Authorizing: "+target+" "+action+" "+qualifier+" "+path);
-		
-		// Superuser
-		Realm<?> realm = principal.getRealm();
-		if (realm.getRoot() == principal) {
-			return AccessDecision.ALLOW;
-		}
-		if (realm.getRoot() instanceof Group && ((Group) realm.getRoot()).isMember(principal)) {
-			return AccessDecision.ALLOW;
-		}
-		
-		// Own permissions and implies
-		for (Permission p: filterAndSortPermissions(principal.getPermissions(), target)) {			
-			String qualifiedPath = path;
-			if (qualifier!=null) {
-				if (!qualifiedPath.endsWith(SLASH)) {
-					qualifiedPath+=SLASH;
-				}
-				qualifiedPath+=qualifier;
-			}
-			AccessDecision accessDecision = p.authorize(securityPolicy, context, target, action, qualifiedPath, environment);
-			if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
-				return accessDecision;
-			}
-			
-			if (realm instanceof SecurityPolicy) {
-				AccessDecision pdAccessDecision = p.authorize((SecurityPolicy) realm, context, target, action, qualifiedPath, environment);
-				if (!AccessDecision.ABSTAIN.equals(pdAccessDecision)) {
-					return pdAccessDecision;
-				}					
-			}
-			
-			if (realm instanceof ActionContainer) {
-				AccessDecision pdAccessDecision = p.authorize(asSecurityPolicy((ActionContainer) realm), context, target, action, qualifiedPath, environment);
-				if (!AccessDecision.ABSTAIN.equals(pdAccessDecision)) {
-					return pdAccessDecision;
-				}					
-			}
-			
-		}
-		
-		// Implies from the permissions annotation
-		if (target instanceof EObject) {
-			List<EAnnotation> permissionsAnnotations = new ArrayList<EAnnotation>();
-			EClass eClass = ((EObject) target).eClass();
-			EAnnotation pa = eClass.getEAnnotation(ANNOTATION_PERMISSIONS);
-			if (pa!=null) {
-				permissionsAnnotations.add(pa);
-			}
-			for (EClass st: eClass.getEAllSuperTypes()) {
-				pa = st.getEAnnotation(ANNOTATION_PERMISSIONS);
-				if (pa!=null) {
-					permissionsAnnotations.add(pa);
-				}				
-			}
-			for (EAnnotation permissionsAnnotation: permissionsAnnotations) {
-				for (Entry<String, String> de: permissionsAnnotation.getDetails().entrySet()) {
-					for (String implied: de.getValue().split("\\r?\\n")) {
-						if (!CoreUtil.isBlank(implied) && !implied.trim().startsWith("#")) {
-							int idx = implied.indexOf(SLASH);
-							String impliedAction = (idx==-1 ? implied : implied.substring(0, idx)).trim();
-							if ("*".equals(impliedAction) || impliedAction.equals(action)) {
-								String impliedPattern = idx==-1 ? SLASH : implied.substring(idx).trim();
-								String fullPath = path.endsWith(SLASH) ? path+qualifier : path+SLASH+qualifier;
-								if (Pattern.matches(impliedPattern, fullPath)) {
-									int iidx = de.getKey().indexOf(SLASH);
-									String implyingAction = (iidx==-1 ? de.getKey() : de.getKey().substring(0, iidx)).trim();
-									String implyingQualifier = iidx==-1 || iidx==de.getKey().length()-1 ? null : de.getKey().substring(iidx+1);
-									AccessDecision implyingAccessDecision = authorize(securityPolicy, principal, context, target, implyingAction, implyingQualifier, SLASH, environment, traversed);
-									if (AccessDecision.ALLOW.equals(implyingAccessDecision)) {
-										return implyingAccessDecision;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		if (traversed.add(principal)) { // prevention of infinite loops if groups are cyclically nested.
-			// Groups
-			for (Group g: principal.getMemberOf()) {
-				AccessDecision accessDecision = authorize(securityPolicy, g, context, target, action, qualifier, path, environment, traversed);
-				if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
-					return accessDecision;
-				}			
-			}
-			
-			// Everyone
-			if (principal instanceof User) {
-				if (!principal.equals(realm.getUnauthenticatedPrincipal())) {
-					Group everyoneGroup = realm.getEveryoneGroup();
-					if (everyoneGroup!=null) {
-						AccessDecision accessDecision = authorize(securityPolicy, everyoneGroup, context, target, action, qualifier, path, environment, traversed);
-						if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
-							return accessDecision;
-						}
-					}
-				}
-			}
-		}
-
-		// Containment
-		if (target instanceof EObject) {
-			EObject targetContainer = ((EObject) target).eContainer();
-			EStructuralFeature targetContainingFeature = ((EObject) target).eContainingFeature();
-			if (targetContainer!=null && targetContainingFeature!=null) {
-				if (qualifier!=null && qualifier.startsWith(PARENT_NAVIGATION)) {
-					qualifier = qualifier.substring(PARENT_NAVIGATION.length());
-				} else {
-					if (path.equals(SLASH)) {
-						path=SLASH+targetContainingFeature.getName();
-					} else {
-						path=SLASH+targetContainingFeature.getName()+path;
-					}
-				}
-				AccessDecision accessDecision = authorize(securityPolicy, principal, context, targetContainer, action, qualifier, path, environment, traversed);
-				if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
-					return accessDecision;
-				}
-			}
-		}
-		
-		return AccessDecision.ABSTAIN;
-	}
-
-	private static List<Permission> filterAndSortPermissions(EList<Permission> permissions, Object target) {
-		// Global permissions
-		if (target==null) { 
-			List<Permission> ret = new ArrayList<>();
-			for (Permission p: permissions) {
-				if (CoreUtil.isBlank(p.getTargetNamespaceURI()) && CoreUtil.isBlank(p.getTargetClass())) {
-					ret.add(p);
-				}
-			}	
-			return ret;
-		}
-		
-		// Class permissions
+	/**
+	 * Sorts permissions in the order of inheritance distance of the permission's action EClass to the target EClass.
+	 * @param permissions
+	 * @param target
+	 * @return
+	 */
+	private static List<Permission> filterAndSortPermissions(EList<Permission> permissions, EObject target) {
 		class PermissionEntry implements Comparable<PermissionEntry> {
 			Permission permission;
 			int distance;
@@ -200,6 +64,7 @@ public class AuthorizationHelper {
 				this.permission = permission;
 				this.distance = distance;
 			}
+			
 			@Override
 			public int compareTo(PermissionEntry o) {
 				if (permission.getTarget()==o.permission.getTarget()) {
@@ -219,22 +84,14 @@ public class AuthorizationHelper {
 			}
 		}
 		List<PermissionEntry> tmp = new ArrayList<>();
-		if (target instanceof EObject) {
-			EClass eClass = target instanceof EClass ? (EClass) target : ((EObject) target).eClass();
-			for (Permission p: permissions) {
-				int distance = (p.getTarget()!=null && p.getTarget().equals(target)) ? 0 : distance(p, eClass);
+		EClass targetClass = target.eClass(); 
+		for (Permission p: permissions) {
+			if (p.getTarget().equals(target)) {
+				int distance = distance(targetClass, getActionClass(p.getAction(), targetClass));
 				if (distance!=-1) {
 					tmp.add(new PermissionEntry(p, distance));
 				}
 			}
-		} else {
-			Class<? extends Object> targetClass = target instanceof Class ? (Class<?>) target : target.getClass();
-			for (Permission p: permissions) {
-				int distance = (p.getTarget()!=null && p.getTarget().equals(target)) ? 0 : distance(p, targetClass);
-				if (distance!=-1) {
-					tmp.add(new PermissionEntry(p, distance));
-				}
-			}			
 		}
 		Collections.sort(tmp);
 		List<Permission> ret = new ArrayList<>();
@@ -243,153 +100,125 @@ public class AuthorizationHelper {
 		}
 		return ret;
 	}
-
-	private static int distance(Permission p, EClass eClass) {
-		if (eClass.getName().equals(p.getTargetClass()) && eClass.getEPackage().getNsURI().equals(p.getTargetNamespaceURI())) {
-			return 0;
-		}
-		for (EClass sc: eClass.getESuperTypes()) {
-			int sd = distance(p, sc);
-			if (sd!=-1) {
-				return sd+1;
-			}
-		}
-		return -1;
-	}
-	
-	private static String[] classQualifiedName(Class<?> clazz) {
-		String className = clazz.getName();
-		int idx = className.lastIndexOf('.');
-		if (idx==-1) {
-			return new String[] {"java://default", className};
-		}
-		return new String[] {"java://"+className.substring(0, idx), className.substring(idx+1)};
-	}
-
-	private static int distance(Permission p, Class<?> clazz) {
-		if (clazz==null) {
-			return -1;
-		}
-		String[] qName = classQualifiedName(clazz);
-		if (qName[1].equals(p.getTargetClass()) && qName[0].equals(p.getTargetNamespaceURI())) {
-			return 0;
-		}
-		int sd = distance(p, clazz.getSuperclass());
-		if (sd!=-1) {
-			return sd;
-		}
-		for (Class<?> i: clazz.getInterfaces()) {
-			int id = distance(p, i);
-			if (id!=-1) {
-				return id+1;
-			}
-		}
-		return -1;
-	}
 	
 	/**
-	 * Inherits condition.
-	 * @return
+	 * Traverses target class supertypes to find a supertype matching action's containing class and package.
+	 * @param action
+	 * @param targetClass
+	 * @return Action class or null.
 	 */
-	public static String effectiveTargetNamespaceURI(Action action) {
-		for (Action a = action; a.eContainer() instanceof Action; a = (Action) a.eContainer()) {
-			String ret = a.getTargetNamespaceURI();
-			if (!CoreUtil.isBlank(ret)) {
-				return ret;
+	private static EClass getActionClass(Action action, EClass targetClass) {
+		EObject container = action.eContainer();
+		while (container instanceof Action) {
+			container = container.eContainer();
+		}
+		Class cls = (Class) container;
+		Package pkg = (Package) cls.eContainer();
+		if (targetClass.getName().equals(cls.getName()) && targetClass.getEPackage().getNsURI().equals(pkg.getNsURI())) {
+			return targetClass;
+		}
+		for (EClass sc: targetClass.getEAllSuperTypes()) {
+			if (sc.getName().equals(cls.getName()) && sc.getEPackage().getNsURI().equals(pkg.getNsURI())) {
+				return sc;
 			}
 		}
-		return action.getTargetNamespaceURI();
-	}
 		
-	/**
-	 * Inherits condition.
-	 * @return
-	 */
-	public static String effectiveTargetClass(Action action) {
-		for (Action a = action; a.eContainer() instanceof Action; a = (Action) a.eContainer()) {
-			String ret = a.getTargetClass();
-			if (!CoreUtil.isBlank(ret)) {
-				return ret;
-			}
-		}
-		return action.getTargetClass();
-	}
-
-	public static SecurityPolicy asSecurityPolicy(final ActionContainer actionContainer) {
-		return new SecurityPolicy() {
-			
-			private void getGrantableActions(Action action, String targetNamespaceURI, String targetClass, Collection<Action> collector) {
-				if (action.isGrantable()
-						&& equal(effectiveTargetNamespaceURI(action), targetNamespaceURI)
-						&& equal(effectiveTargetClass(action), targetClass)) {
-					collector.add(action);
-				}
-				for (Action c: action.getActions()) {
-					getGrantableActions(c, targetNamespaceURI, targetClass, collector);
-				}
-			}
-			
-			@Override
-			public Collection<Action> getGrantableActions(String targetNamespaceURI, String targetClass) {
-				List<Action> ret = new ArrayList<>();
-				if (actionContainer instanceof Action 
-						&& ((Action) actionContainer).isGrantable()
-						&& equal(effectiveTargetNamespaceURI((Action) actionContainer), targetNamespaceURI)
-						&& equal(effectiveTargetClass((Action) actionContainer), targetClass)) {
-					ret.add((Action) actionContainer);
-				}
-				for (Action a: actionContainer.getActions()) {
-					getGrantableActions(a, targetNamespaceURI, targetClass, ret);
-				}
-				return ret;
-			}
-			
-			@Override
-			public Action getAction(ActionKey actionKey) {
-				return findAction(actionKey, actionContainer, null, null, null);
-			}
-			
-		};
-	}	
-	
-	private static boolean equal(String str1, String str2) {
-		if (CoreUtil.isBlank(str1)) {
-			return CoreUtil.isBlank(str2);
-		}
-		if (CoreUtil.isBlank(str2)) {
-			return false;
-		}
-		return str1.trim().equals(str2.trim());
-	}
-
-	public static Action findAction(
-			ActionKey actionKey, 
-			ActionContainer ac, 
-			String inheritedNamespace, 
-			String inheritedClass, 
-			String inheritedName) {
-		for (Action action: ac.getActions()) {
-//			System.out.println(actionKey.getName());
-			String effectiveNamespace = effective(action.getTargetNamespaceURI(), inheritedNamespace);
-			String effectiveClass = effective(action.getTargetClass(), inheritedClass);
-			String effectiveName = effective(action.getName(), inheritedName);
-			if (equal(actionKey.getTargetNamespaceURI(), effectiveNamespace)
-					&& equal(actionKey.getTargetClass(), effectiveClass)
-					&& ("*".equals(effectiveName) || equal(actionKey.getName(), effectiveName)) 
-					&& equal(actionKey.getQualifier(), action.getQualifier())) {
-				return action;
-			}
-			Action subAction = findAction(actionKey, action, effectiveNamespace, effectiveClass, effectiveName);
-			if (subAction!=null) {
-				return subAction;
-			}
-		}
 		return null;
 	}
-	
-	private static String effective(String val, String inherited) {
-		return CoreUtil.isBlank(val) ? inherited : val;
-	}
 
+	/**
+	 * Inheritance distance between two classes.
+	 * @param subClass
+	 * @param superClass
+	 * @return
+	 */
+	private static int distance(EClass subClass, EClass superClass) {
+		if (superClass != null && superClass.isSuperTypeOf(subClass)) {
+			if (subClass.equals(superClass)) {
+				return 0;
+			}
+			for (EClass sc: subClass.getESuperTypes()) {
+				int sd = distance(sc, sc);
+				if (sd!=-1) {
+					return sd+1;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	private static AccessDecision authorize(
+			Principal principal, 
+			Context context,
+			EObject target, 
+			String action,
+			String qualifier, 
+			List<EStructuralFeature> path, 
+			Map<String, Object> environment, 
+			Set<Principal> traversed) {
+		
+//		System.out.println("Authorizing: "+target+" "+action+" "+qualifier+" "+path);
+		
+		// Root
+		Realm<?> realm = principal.getRealm();
+		if (realm.getRoot() == principal) {
+			return AccessDecision.ALLOW;
+		}
+		if (realm.getRoot() instanceof Group && ((Group) realm.getRoot()).isMember(principal)) {
+			return AccessDecision.ALLOW;
+		}
+		
+		// Own permissions and implies
+		for (Permission p: filterAndSortPermissions(principal.getPermissions(), target)) {
+			StringBuilder qualifierBuilder = new StringBuilder();
+			for (EStructuralFeature pe: path) {
+				qualifierBuilder.append(pe.getName());
+				qualifierBuilder.append("/");
+			}
+			qualifierBuilder.append(qualifier);
+			AccessDecision accessDecision = p.authorize(context, action, qualifierBuilder.toString(), environment);
+			if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
+				return accessDecision;
+			}			
+		}
+				
+		if (traversed.add(principal)) { // prevention of infinite loops if groups are cyclically nested.
+			// Groups
+			for (Group g: principal.getMemberOf()) {
+				AccessDecision accessDecision = authorize(g, context, target, action, qualifier, path, environment, traversed);
+				if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
+					return accessDecision;
+				}			
+			}
+			
+			// Everyone
+			Principal everyone = principal.getRealm().getEveryone();
+			if (everyone != null && principal.getRealm().getGuest() != principal) {
+				AccessDecision accessDecision = authorize(everyone, context, target, action, qualifier, path, environment, traversed);
+				if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
+					return accessDecision;
+				}
+			}
+		}
+
+		// Containment
+		if (target instanceof EObject) {
+			EObject targetContainer = ((EObject) target).eContainer();
+			EStructuralFeature targetContainingFeature = ((EObject) target).eContainingFeature();
+			if (targetContainer!=null && targetContainingFeature!=null) {
+				if (qualifier!=null && qualifier.startsWith(PARENT_NAVIGATION)) {
+					qualifier = qualifier.substring(PARENT_NAVIGATION.length());
+				} else {
+					path.add(0, targetContainingFeature);
+				}
+				AccessDecision accessDecision = authorize(principal, context, targetContainer, action, qualifier, path, environment, traversed);
+				if (!AccessDecision.ABSTAIN.equals(accessDecision)) {
+					return accessDecision;
+				}
+			}
+		}
+		
+		return AccessDecision.ABSTAIN;
+	}
 	
 }
