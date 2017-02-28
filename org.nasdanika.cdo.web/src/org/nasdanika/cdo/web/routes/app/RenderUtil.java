b4d03d7988dd03587161286bb51c5e5a2abb46e8
Pavel <Pavel.Vlasov@nasdanika.org>
2017-02-28 17:15:35 -0500

    Helper class.

---
+++ org.nasdanika.cdo.web/src/org/nasdanika/cdo/web/routes/app/RenderUtil.java
@@ -0,0 +1,35 @@
+package org.nasdanika.cdo.web.routes.app;
+
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * Utility class for the {@link Renderer}
+ * @author Pavel
+ *
+ */
+class RenderUtil {
+	
+	// Utility class, no instances.
+	private RenderUtil() {
+		throw new UnsupportedOperationException();
+	}
+	
+	static String getRenderAnnotation(EClass eClass, String key) {		
+		EAnnotation ra = eClass.getEAnnotation(Renderer.RENDER_ANNOTATION_SOURCE);
+		if (ra != null) {
+			String value = ra.getDetails().get(key);
+			if (value != null) {
+				return value;
+			}
+		}
+		for (EClass st: eClass.getESuperTypes()) {
+			String raStr = getRenderAnnotation(st, key);
+			if (raStr != null) {
+				return raStr;
+			}
+		}
+		return null;
+	}	
+
+}
