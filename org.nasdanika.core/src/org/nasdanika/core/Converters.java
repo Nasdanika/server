56055a0bf19e975b249f4cda0fd91e6c14e4575c
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2017-03-20 15:50:28 -0400

    Conversion to string from URL, InputStream, and Reader.

--- org.nasdanika.core/src/org/nasdanika/core/Converters.java
+++ org.nasdanika.core/src/org/nasdanika/core/Converters.java
@@ -1,11 +1,29 @@
 package org.nasdanika.core;
 
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
 
 public class Converters extends ReflectiveConverterProvider {
 
 	public Converters() throws Exception {
 		super();
 	}
+	
+	@ConverterMethod
+	public String convert(URL value, Context context) {
+		return CoreUtil.stringify(value);
+	}	
+
+	@ConverterMethod
+	public String convert(InputStream value, Context context) {
+		return CoreUtil.stringify(value);
+	}	
+
+	@ConverterMethod
+	public String convert(Reader value, Context context) {
+		return CoreUtil.stringify(value);
+	}	
 
 	@ConverterMethod
 	public String toString(Object value, Context context) {
