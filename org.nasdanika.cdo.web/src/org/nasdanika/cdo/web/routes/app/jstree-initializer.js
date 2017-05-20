d95e13955e1cca7f1b489d0b28ae205dc0463ac3
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2017-05-20 11:04:29 -0400

    jsTree search plug-in.

--- org.nasdanika.cdo.web/src/org/nasdanika/cdo/web/routes/app/jstree-initializer.js
+++ org.nasdanika.cdo.web/src/org/nasdanika/cdo/web/routes/app/jstree-initializer.js
@@ -1,12 +1,31 @@
-$('#{{container-id}}').jstree({ 
-	'core' : { 
-		'data' : {{data}}, 
-		"multiple" : false 
-	}, 
-	'plugins' : ['state', 'sort'],
-	'state' : { 
-		"filter" : function (k) { 
-			delete k.core.selected; return k; 
-		} 
-	}
+$(function () {
+	$('#{{container-id}}').jstree({ 
+		'core' : { 
+			'data' : {{data}}, 
+			"multiple" : false 
+		}, 
+		'plugins' : ['state', 'sort', 'search'],
+		'state' : { 
+			"filter" : function (k) { 
+				delete k.core.selected; return k; 
+			} 
+		},
+		'search' : {
+			show_only_matches : true,
+			show_only_matches_children : true
+		}
+	});
+	
+	var to = false;
+
+	$('#{{container-id}}-search').keyup(function () {
+		if (to) { 
+			clearTimeout(to); 
+		}
+		to = setTimeout(function () {
+			var v = $('#{{container-id}}-search').val();
+			$('#{{container-id}}').jstree(true).search(v);
+		}, 250);
+  });	
+	
 });
