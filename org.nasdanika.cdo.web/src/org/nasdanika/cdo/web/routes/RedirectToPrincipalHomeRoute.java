9af12363941003809af196e8e54d83db00b4e3cd
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2017-07-14 12:33:02 -0400

    Handles user created in the current transaction - e.g. auto-created.

--- org.nasdanika.cdo.web/src/org/nasdanika/cdo/web/routes/RedirectToPrincipalHomeRoute.java
+++ org.nasdanika.cdo.web/src/org/nasdanika/cdo/web/routes/RedirectToPrincipalHomeRoute.java
@@ -1,5 +1,12 @@
 package org.nasdanika.cdo.web.routes;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.emf.cdo.transaction.CDOCommitContext;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.cdo.transaction.CDOTransactionHandler2;
 import org.nasdanika.cdo.CDOViewContext;
 import org.nasdanika.cdo.security.Principal;
 import org.nasdanika.web.Action;
@@ -13,13 +20,48 @@ import org.nasdanika.web.Route;
  */
 public class RedirectToPrincipalHomeRoute implements Route {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public Action execute(HttpServletRequestContext context, Object... args) throws Exception {
 		if (context instanceof CDOViewContext) {
 			for (Principal principal: ((CDOViewContext<?,?>) context).getPrincipals()) {
-				String principalHome = context.getObjectPath(principal)+"/home.html";
-				String queryString = context.getRequest().getQueryString();
-				context.getResponse().sendRedirect(queryString == null ? principalHome : principalHome + "?" + queryString);
+				if (((CDOViewContext<?,?>) context).getView() instanceof CDOTransaction && principal.cdoID().isTemporary()) {
+					((CDOViewContext<CDOTransaction,?>) context).getView().addTransactionHandler(new CDOTransactionHandler2() {
+
+						@Override
+						public void committedTransaction(CDOTransaction transaction, CDOCommitContext commitContext) {
+							try {
+								String principalHome = context.getObjectPath(principal)+"/home.html";
+								String queryString = context.getRequest().getQueryString();
+								context.getResponse().sendRedirect(queryString == null ? principalHome : principalHome + "?" + queryString);
+							} catch (Exception e) {
+								e.printStackTrace();
+								try {
+									context.getResponse().sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+								} catch (IOException ioe) {
+									ioe.printStackTrace();
+								}
+							}
+						}
+
+						@Override
+						public void committingTransaction(CDOTransaction transaction, CDOCommitContext commitContext) {
+							// NOP
+							
+						}
+
+						@Override
+						public void rolledBackTransaction(CDOTransaction transaction) {
+							// NOP
+							
+						}
+						
+					});
+				} else {
+					String principalHome = context.getObjectPath(principal)+"/home.html";
+					String queryString = context.getRequest().getQueryString();
+					context.getResponse().sendRedirect(queryString == null ? principalHome : principalHome + "?" + queryString);					
+				}
 				return Action.NOP;
 			}
 		}
