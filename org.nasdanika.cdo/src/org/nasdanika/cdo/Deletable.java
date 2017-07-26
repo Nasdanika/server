fe0d02ea03096bb119ba842fd9c8aeb75d66074f
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2017-07-25 20:45:10 -0400

    Default implementation of delete()

--- org.nasdanika.cdo/src/org/nasdanika/cdo/Deletable.java
+++ org.nasdanika.cdo/src/org/nasdanika/cdo/Deletable.java
@@ -1,16 +1,24 @@
-package org.nasdanika.core;
+package org.nasdanika.cdo;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 /**
  * Object which handles its own deletion from the repository.
  * This interface is used by CDOViewRoute to process <code>$delete</code> invocation. 
  * For instances of <code>Deletable</code> their <code>delete()</code> method is invoked. 
- * Otherwise <code>EcoreUtil.delete(obj, true)</code> is invoked 
+ * Otherwise <code>EcoreUtil.delete(obj, true)</code> is invoked.
+ * 
  * @author Pavel
  *
  * @param <C>
  */
-public interface Deletable<C extends Context> {
+public interface Deletable<C extends CDOTransactionContext<?>> {
 	
-	void delete(C context) throws Exception;
+	default void delete(C context) throws Exception {
+		if (this instanceof EObject) {
+			EcoreUtil.delete((EObject) this);
+		}
+	};
 
 }
